a46c7179a77adc5af129f0838fcc47daf3362fda
jihwan2da <wlghks8982@naver.com>
2021-12-28 19:32:57 +0900

    upload comment

---
+++ domain/src/main/kotlin/sample/jpa/comment/exception/CommentNotExistException.kt
@@ -0,0 +1,3 @@
+package sample.jpa.comment.exception
+
+class CommentNotExistException : RuntimeException("존재하지 않는 댓글입니다.")
---
+++ domain/src/main/kotlin/sample/jpa/comment/model/repository/CommentRepository.kt
@@ -0,0 +1,9 @@
+package sample.jpa.comment.model.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+import sample.jpa.comment.model.entitiy.Comment
+
+@Repository
+interface CommentRepository:JpaRepository<Long,Comment> {
+}
